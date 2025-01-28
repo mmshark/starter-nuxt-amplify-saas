bb07dfab1d827ade22a93d1876f45b07fefdb3ec
Albert Puigsech <albert@puigsech.com>
2025-01-28 15:22:31 +0100

    Add both PlanCards component as a test

--- pages/index.vue
+++ pages/index.vue
@@ -11,20 +11,39 @@
     </div>
 
     <div class="mb-8">
+      <p>Plan Vanila Cards</p>
       <PlanVanilaCards price-switcher />
     </div>
 
     <div>
+      <p>Plan Stripe Cards</p>
       <PlanStripeCards 
         pricing-table-id="prctbl_1QmE1UIOUVeEvI6sJki3lK0x"
-        publishable-key="pk_test_51QmDrCIOUVeEvI6skZG6L0cGgLcoD8EJXKcMQL1ebLoX3jOu8FnSr5poK3Pkra8GgVbkijMiNk0DKI0p0kPEhXdm00RGd9HBDG">
+        publishable-key="pk_test_51QmDrCIOUVeEvI6skZG6L0cGgLcoD8EJXKcMQL1ebLoX3jOu8FnSr5poK3Pkra8GgVbkijMiNk0DKI0p0kPEhXdm00RGd9HBDG"
+        :customer-email="user?.signInDetails?.loginId"
+        :client-reference-id="user?.userId">
       </PlanStripeCards>
     </div>
   </div>
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue';
+import { Amplify } from 'aws-amplify';
+import { getCurrentUser } from 'aws-amplify/auth';
+import outputs from '../amplify_outputs.json';
 
+Amplify.configure(outputs);
+const user = ref(null);
+
+onMounted(async () => {
+  try {
+    user.value = await getCurrentUser();
+    console.log("User:", user.value);
+  } catch (error) {
+    console.error("Error getting current user:", error);
+  }
+});
 </script>
 
 <style scoped>
